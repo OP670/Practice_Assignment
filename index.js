b1f01320d0d8125bcccf2fc0ae5726068a7cbebf
OP670 <obicanprolaznik@gmail.com>
2022-03-21 15:40:34 +0100

    Add file processing functionality, example text file and config file

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import url from 'url';
+import fs from 'fs';
+
 const app = express();
 const PORT = 8080;
+var confData = {};
+var text = "";
 
 app.use(bodyParser.json());
+fs.readFile('config.json','utf8',(err,data)=>{
+    if (err) {
+        console.log(err);
+    } else {
+        confData = JSON.parse(data);
+        //console.log(confData.readFrom);
+        fs.readFile(confData.readFrom,'utf8',(err,data)=>{
+            if (err) {
+                console.log(err);
+            } else {
+                text = data;
+                text = text.split(/\r?\n/);
+                console.log(text);
+            }
+        });
+    }
+});
 
 
 app.get('/', (req, res) => {
@@ -18,8 +39,25 @@ app.get('/count', (req, res) => {
     const urlObject = url.parse(req.url, true).query;
     if ('word' in urlObject){
         //add word counting logic here
-        //return JSON object with word, sentences and word counts
-        res.send("You were looking for "+ urlObject.word);
+        var resObj = {"word":urlObject.word, "sentances":[]};
+        var pattern = new RegExp(urlObject.word,'gi');
+        var max = 0;
+        var line = "";
+        for(line of text){
+            var count = line.match(pattern);
+            if (count) count = count.length;
+            console.log(line)
+            if (count>0){
+                resObj.sentances.push({"sentance":line, "count":count});
+            }
+            resObj.sentances.sort((a, b) => {
+                if (parseInt(b.count) - parseInt(a.count) == 0) return a.sentance > b.sentance?1:-1;
+                return parseInt(b.count) - parseInt(a.count);
+            });
+        }
+        
+        res.send("<pre>"+JSON.stringify(resObj,null,4)+"</pre>");
+        
     } else {
         res.status(404).send("Sorry, we couldn't find what you were looking for...")
     }
